ea080af0e735557b7e8417510a01aaa53cfa4c74
Faudev <fauezo@gmail.com>
2023-10-28 09:34:52 -0300

    Add project files.

---
+++ AprovacaoDeAlunos/Aluno.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+
+namespace AprovacaoDeAlunos
+{
+    internal class Aluno
+    {
+        public string Nome;
+        public double NotaA;
+        public double NotaB;
+        public double NotaC;
+
+        public double Media()
+        {
+            return (NotaA+NotaB+NotaC)/3;
+
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+}
---
+++ AprovacaoDeAlunos/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace AprovacaoDeAlunos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno = new Aluno();
+            bool sair = true;
+           
+            while (sair = true) { 
+            Console.Write("Digite o nome do Aluno: ");
+            aluno.Nome = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Digite as 3 notas dos Aluno");
+            aluno.NotaA = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            aluno.NotaB = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            aluno.NotaC = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            Console.WriteLine();
+            Console.WriteLine("Nota Final = " + aluno.Media().ToString("f2", CultureInfo.InvariantCulture));
+            Console.WriteLine();
+            if (aluno.Media() > 50.00)
+            {
+                Console.WriteLine("Aprovador");
+            }
+            else
+            {
+                Console.WriteLine("Reprovador");
+            }
+                Console.Write("Novo Dados [1] Novo ou [0] Sair: ");
+                string s = Console.ReadLine();
+                Console.Clear();
+                if (s == "0" ) 
+                { 
+                   break; 
+                    Console.Clear();
+                } else 
+                {
+                    Console.Write("informe Dados [1] Novo ou [0] Sair: ");
+                    s = Console.ReadLine();
+                }               
+            }
+            Console.WriteLine("Ate logo");
+            Console.WriteLine("\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        }
+
+
+    }
+}
